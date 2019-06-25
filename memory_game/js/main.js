23ca4258740b66121d423b1258589814bb8c70bb
Carramah J Quiett <carramahjquiett@Carramahs-MacBook-Pro.local>
2019-06-24 23:33:23 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,20 @@
 console.log("Up and running!");
 var cards = ["king", "queen", "king", "queen"];
-cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped a " + cardOne + "!");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped a " + cardTwo + "!");
-if (cardsInPlay.length === 2) 
-if (cardsInPlay[0] === cardsInPlay[1]) 
-{console.log("You've found a match!");} 
-else if (cardsInPlay[0] !== cardsInPlay[1])
-{console.log("Sorry, try again.");}
+
+var cardsInPlay = [];
+var cardId = [];
+
+function checkForMatch() {if 
+(cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}}
+
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);}
+
+flipCard(0);
+flipCard(1);
+checkForMatch();	
