72d908fcff945ce62daed22da3de6823a9e58a60
Carramah J Quiett <carramahjquiett@Carramahs-MacBook-Pro.local>
2019-07-07 20:49:05 -0400

    finally fisned created game logic. I hope you'll excpeted it. Thanks

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -13,7 +13,7 @@ cardImage: "images/queen-of-diamonds.png"
 {
 rank: "king",
 suit: "hearts",
-cardImage: "images/king-of-hearts.png"	
+cardImage: "images/king-of-hearts.png"
 },
 {
 rank: "king",
@@ -23,24 +23,35 @@ cardImage: "images/king-of-diamonds.png"
 ];
 
 var cardsInPlay = [];
-var cardId = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
     } else {
   alert("Sorry, try again.");
-    } 
+    }
 };
 
-function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId].rank);
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+function flipCard() {
+		var cardId = this.getAttribute('data-id');
+    cardsInPlay.push (cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
+		if (cardsInPlay.length === 2) {
+			checkForMatch();
+		};
+		console.log("User flipped " + cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 };
 
-checkForMatch();
-flipCard(0);
-flipCard(2);
-	
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
